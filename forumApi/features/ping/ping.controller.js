fdc87cf79314ec6e9ad6dc2cd53959c80d88d664
Joss-inf <jossbessi460@gmail.com>
2025-10-19 21:50:54 +0200

    api v1.0.10

--- forumApi/features/ping/ping.controller.js
+++ forumApi/features/ping/ping.controller.js
@@ -1,12 +1,7 @@
-
-/**
- * Handles a ping request and responds with a JSON message 'pong'.
- * 
- * @async
- * @function getPing
- * @param {Object} res - Express response object.
- * @returns {void}
- */
-export async function getPing(res){
-    res.status(200).json({ message: 'pong' });
+export async function getPing(_req,res,next){
+    try {
+        res.status(200).json({ message: 'pong' });
+      } catch (error) {
+        next(error);
+    }
 }
