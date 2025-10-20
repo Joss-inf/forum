c9577a872c920d260d675fcee02ed4e7d403e6b1
Joss-inf <jossbessi460@gmail.com>
2025-10-20 22:09:56 +0200

    client v1.0.5

--- forumClient/src/main.ts
+++ forumClient/src/main.ts
@@ -1,27 +1,28 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia';
-import App from './App.vue';
-import router from './router';
-import { useAuthStore } from '@/stores/auth'; 
+import { createApp, nextTick } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import { useAuthStore } from '@/stores/auth'
 import '@/assets/main.css'
 import '@/assets/base.css'
 
-const app = createApp(App);
-const pinia = createPinia();
+const app = createApp(App)
+const pinia = createPinia()
+app.use(pinia)
 
-app.use(pinia);
-const authStore = useAuthStore();
+const authStore = useAuthStore()
 
-authStore.initializeAuth().then(() => {
-  app.use(router);
-  app.mount('#app');
-});
+authStore.initializeAuth().then(async () => {
+  app.use(router)
+  app.mount('#app')
 
-window.addEventListener('load', async () => {
+  // On attend le rendu complet
+  await nextTick()
+
+  // Gestion du preloader
   const preloader = document.getElementById('preloader')
   if (preloader) {
     preloader.classList.add('fade-out')
-    setTimeout(() => preloader.remove(), 100)
+    setTimeout(() => preloader.remove(), 500)
   }
 })
-
